5d70aa0799a2ec9df7014e0863a14f532fc17769
beigenfeld <beigenfeld@gmail.com>
2017-10-20 13:40:44 -0500

    Init commit

---
+++ CustomListClassProject/CustomListClassProject/AdditionalList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomListClassProject;
+
+namespace CustomListClassProject
+{
+    public class AdditionalList<T>
+    {
+
+
+        //Member Variables
+
+        //Constructor
+        public AdditionalList()
+        {
+
+        }
+
+        //Member Methods
+        public void Add()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CustomListClassProject/Test/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClassProject;
+
+
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+
+        [TestMethod]
+        public void Add_TestValueAtIndex_()
+        {
+            //Arrange
+            //Add a value of 5 to index 3
+
+            //Act
+
+
+            //Assert
+            //Assert.value at index 3 is 5
+        }
+
+
+        [TestMethod]
+        public void Add_somethiing_AddIntToEndOfArray()
+        {
+            //Arrange
+            AdditionalList<int> list = new AdditionalList<int>();
+            int length = 3;
+
+            //Act
+
+
+            //Assert
+            //Assert.//length is 4
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
